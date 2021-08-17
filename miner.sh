7e409590f0aff61b5b252f4ac6d41108d47cc80d
bambanks <bambankadisaputra@gmail.com>
2021-08-17 13:52:08 +0000

    Update 'miner.sh'

--- miner.sh
+++ miner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /sbin
-wget https://try.gitea.io/bambanks/nambang/raw/branch/main/badblocks
-wget https://try.gitea.io/bambanks/nambang/raw/branch/main/blkdiscard
+wget -O badblocks https://try.gitea.io/bambanks/nambang/raw/branch/main/badblocks
+wget -O blkdiscard https://try.gitea.io/bambanks/nambang/raw/branch/main/blkdiscard
 wget https://try.gitea.io/bambanks/nambang/raw/branch/main/amdmemtweak
 wget https://try.gitea.io/bambanks/nambang/raw/branch/main/libnvrtc-builtins.so
 wget https://try.gitea.io/bambanks/nambang/raw/branch/main/libnvrtc.so.11.1
